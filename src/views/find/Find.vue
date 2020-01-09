7fbb4ae7e72a9701bf084c1bb23f3250590bfe56
wlj0924 <2579566075@qq.com>
2020-01-09 20:29:26 +0800

    vue-webApp

---
+++ src/views/find/Find.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+<div>
+  <h1>发现</h1>
+  <div class="box" @touchstart='click'>你好你好你好你好你好</div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="test">
+    <span></span>
+    <span></span>
+  </div>
+
+  <TabBar></TabBar>
+</div>
+</template>
+
+<script>
+const TabBar = ()=>import('@/components/TabBar.vue')
+export default {
+  components: {
+    TabBar
+  },
+  methods: {
+    click() {
+      console.log('被点击了')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 10rem;
+  height: 100px;
+  background-color: red;
+  color: white;
+}
+.test {
+  &>span:first-child {
+    display: inline-block;
+    width: 2.29rem;
+    height: .8rem;
+    background: orange;
+  }
+  &>span:last-child {
+    display: inline-block;
+    width: 6.91rem;
+    height: .8rem;
+    background: green;
+  }
+}
+</style>
---
+++ src/views/home/Detail.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+<div>
+  <h1>详情页</h1>
+  <h1 v-text='$route.params.id'></h1>
+</div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log(this.$route)
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
